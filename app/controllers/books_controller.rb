c9a6e569002ffc2b08fb407bd0410605d65e1541
miura-yuto <miura.yuto.infratop@gmail.com>
2021-06-19 04:48:50 +0000

    first

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,7 @@ class BooksController < ApplicationController
 
   def show
     @book_new = Book.new
+    @book_comment = BookComment.new
     @book = Book.find(params[:id])
     @user = @book.user
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get "home/about", to: "homes#about", as: :homes_about
-  get "homes/about"  => "homes#about"
   get "users/show"
   devise_for :users
   root to: "homes#top"
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:create, :index, :show, :edit, :update]
 end
